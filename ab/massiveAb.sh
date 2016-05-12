7d4a123c90769d4381a671669f0fd6ca14d296ab
root <root@i19.informatica.escoladeltreball.org>
2016-05-12 10:18:34 +0200

    update massiveAb.sh

--- ab/massiveAb.sh
+++ ab/massiveAb.sh
@@ -9,18 +9,41 @@
 ### VARIABLES ###
 
 # Requests
-number_req=$1
+number_req=10000
 
 # Concurrency
-number_conc=$2
+number_conc=20
 
 # Port number
-port=$3
+port1=8080
+port2=8081
+port3=8082
+port4=8083
 
 ### MAIN PROGRAM ###
 
 # ab requests
-ab -n $number_req -c $number_conc http://192.168.2.49:$port/
-ab -n $number_req -c $number_conc http://192.168.2.49:$port/sobremi.html
-ab -n $number_req -c $number_conc http://192.168.2.49:$port/contacte.html
-ab -n $number_req -c $number_conc http://192.168.2.49:$port/altresprojectes.html
+
+# Container 1
+ab -n $number_req -c $number_conc http://192.168.2.49:$port1/
+ab -n $number_req -c $number_conc http://192.168.2.49:$port1/sobremi.html
+ab -n $number_req -c $number_conc http://192.168.2.49:$port1/contacte.html
+ab -n $number_req -c $number_conc http://192.168.2.49:$port1/altresprojectes.html
+
+# Container 2
+ab -n $number_req -c $number_conc http://192.168.2.49:$port2/
+ab -n $number_req -c $number_conc http://192.168.2.49:$port2/sobremi.html
+ab -n $number_req -c $number_conc http://192.168.2.49:$port2/contacte.html
+ab -n $number_req -c $number_conc http://192.168.2.49:$port2/altresprojectes.html
+
+# Container 3
+ab -n $number_req -c $number_conc http://192.168.2.49:$port3/
+ab -n $number_req -c $number_conc http://192.168.2.49:$port3/sobremi.html
+ab -n $number_req -c $number_conc http://192.168.2.49:$port3/contacte.html
+ab -n $number_req -c $number_conc http://192.168.2.49:$port3/altresprojectes.html
+
+# Container 4
+ab -n $number_req -c $number_conc http://192.168.2.49:$port4/
+ab -n $number_req -c $number_conc http://192.168.2.49:$port4/sobremi.html
+ab -n $number_req -c $number_conc http://192.168.2.49:$port4/contacte.html
+ab -n $number_req -c $number_conc http://192.168.2.49:$port4/altresprojectes.html
