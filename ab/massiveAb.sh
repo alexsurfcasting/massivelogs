92fc8388b82e868414ed9ab5cde1af3ae5c84394
Alex <alexsurfcasting95@gmail.com>
2016-05-17 09:39:42 +0200

    update script

--- ab/massiveAb.sh
+++ ab/massiveAb.sh
@@ -9,10 +9,14 @@
 ### VARIABLES ###
 
 # Requests
-number_req=10000
+number_req=100
 
 # Concurrency
 number_conc=20
+
+# IP
+
+ip=192.168.2.49
 
 # Port number
 port1=8080
@@ -25,25 +29,25 @@ port4=8083
 # ab requests
 
 # Container 1
-ab -n $number_req -c $number_conc http://192.168.2.49:$port1/
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port1/sobremi.html
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port1/contacte.html
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port1/altresprojectes.html
+ab -n $number_req -c $number_conc http://$ip:$port1/
+ab -n $number_req -c $number_conc http://$ip:$port1/sobremi.html
+ab -n $number_req -c $number_conc http://$ip:$port1/contacte.html
+ab -n $number_req -c $number_conc http://$ip:$port1/altresprojectes.html
 
 # Container 2
-ab -n $number_req -c $number_conc http://192.168.2.49:$port2/
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port2/sobremi.html
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port2/contacte.html
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port2/altresprojectes.html
+ab -n $number_req -c $number_conc http://$ip:$port2/
+ab -n $number_req -c $number_conc http://$ip:$port2/sobremi.html
+ab -n $number_req -c $number_conc http://$ip:$port2/contacte.html
+ab -n $number_req -c $number_conc http://$ip:$port2/altresprojectes.html
 
 # Container 3
-ab -n $number_req -c $number_conc http://192.168.2.49:$port3/
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port3/sobremi.html
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port3/contacte.html
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port3/altresprojectes.html
+ab -n $number_req -c $number_conc http://$ip:$port3/
+ab -n $number_req -c $number_conc http://$ip:$port3/sobremi.html
+ab -n $number_req -c $number_conc http://$ip:$port3/contacte.html
+ab -n $number_req -c $number_conc http://$ip:$port3/altresprojectes.html
 
 # Container 4
-ab -n $number_req -c $number_conc http://192.168.2.49:$port4/
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port4/sobremi.html
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port4/contacte.html
-#~ ab -n $number_req -c $number_conc http://192.168.2.49:$port4/altresprojectes.html
+ab -n $number_req -c $number_conc http://$ip:$port4/
+ab -n $number_req -c $number_conc http://$ip:$port4/sobremi.html
+ab -n $number_req -c $number_conc http://$ip:$port4/contacte.html
+ab -n $number_req -c $number_conc http://$ip:$port4/altresprojectes.html
